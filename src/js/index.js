d448dad3176398aa6b5b7cd1d9d11813cf204b02
rk <103226779+rkbno@users.noreply.github.com>
2023-06-25 22:49:08 -0300

    ajustes

--- src/js/index.js
+++ src/js/index.js
@@ -161,6 +161,7 @@ const cont = document.querySelector('.contaner')
 const btnWrapper = document.querySelector('.btn-wrapper')
 // 2 - quando clicar no botão fazer ele executar a ação de mostrar o card e modificar o backgroud
 btn.addEventListener('click', handClick)
+btn.style.fontFamily = 'Press Start 2P, Arial, Helvetica, sans-serif;'
     function handClick()  {
         btn.remove();
         cont.style.width = '0';
